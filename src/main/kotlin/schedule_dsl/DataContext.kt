99092000346f504abd24ec62b23858d12e0b7dd6
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-12-01 21:40:18 +0300

    add fun startFrom

--- src/main/kotlin/schedule_dsl/DataContext.kt
+++ src/main/kotlin/schedule_dsl/DataContext.kt
@@ -4,6 +4,7 @@ import schedule_dsl.models.Student
 import schedule_dsl.models.Subject
 import schedule_dsl.models.Teacher
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class DataContext {
 
@@ -15,6 +16,10 @@ class DataContext {
 
     private var startTime = LocalTime.of(8, 0)
 
+
+    fun startFrom(time: String) {
+        startTime = LocalTime.from(DateTimeFormatter.ISO_LOCAL_TIME.parse(time))
+    }
 
     fun buildDataSet() = DataSet().apply {
         teachers.addAll(this@DataContext.teachers)
